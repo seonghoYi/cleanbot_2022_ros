ecefe2cff5ace6d2b2520833c6ae0bf35d489c88
seonghoYi <shLee.4781@gmail.com>
2021-07-20 16:09:34 +0900

    Modify functions for new hardware

--- src/serial_node.cpp
+++ src/serial_node.cpp
@@ -6,35 +6,25 @@ void controlMotor(float left_speed, float right_speed);
 
 void commandVelocityCallback(const geometry_msgs::Twist &cmd_vel)
 {
-    
-    const float ROBOT_WIDTH = 0.27f;
+    const float ROBOT_WIDTH = 0.35f;
     //ROS_INFO("cmd_vel.linear.x: %f, cmd_vel.angular.z * ROBOT_WIDTH / 2: %f\n", cmd_vel.linear.x, cmd_vel.angular.z * ROBOT_WIDTH / 2);
     float left_speed_out = cmd_vel.linear.x - cmd_vel.angular.z * ROBOT_WIDTH / 2; //v=rw 선속도=반지름*각속도
     float right_speed_out = cmd_vel.linear.x + cmd_vel.angular.z * ROBOT_WIDTH / 2;
-    ROS_INFO("left speed: %fm/s, right_speed: %fm/s\n", left_speed_out, right_speed_out);
+    //ROS_INFO("left speed: %fm/s, right_speed: %fm/s\n", left_speed_out, right_speed_out);
     controlMotor(left_speed_out, right_speed_out);
 }
 
 void controlMotor(float left_speed, float right_speed)
 {
     //static Controller device(port, baudrate, 128);
-
-    const long F_CPU = 16000000UL;
-    const int CLK_DIV = 256;
-    const float MAX_SPEED = 0.376f; //m/s
-    const float PHI = 0.066f; // m
-    const float STEP_ANGLE = 0.9f; // half step
-
+    //const float MAX_SPEED = 0.376f; //m/s
+    const float PI = 3.1415926;
+    const float WHEEL_WIDTH = 0.07f;
+    
     bool L_dir;
     bool R_dir;
     float L_rpm;
     float R_rpm;
-    float L_pulse_freq;
-    float R_pulse_freq;
-
-
-    uint8_t L_ocr(0);
-    uint8_t R_ocr(0);
 
     
     if ((int)(left_speed * 1000000) == 0 || (int)(right_speed * 1000000) == 0)
@@ -43,36 +33,31 @@ void controlMotor(float left_speed, float right_speed)
         return;
     }
 
-    L_rpm = (abs(left_speed) * 60) / (PHI * M_PI); // 분속 / 바퀴 지름 = rpm
-    R_rpm = (abs(right_speed) * 60) / (PHI * M_PI);
-
-    L_pulse_freq = L_rpm / (STEP_ANGLE / 6); // rpm = (1/6)*θ*P
-    R_pulse_freq = R_rpm / (STEP_ANGLE / 6);
-
-    L_ocr = (int)(F_CPU / (2 * CLK_DIV * L_pulse_freq));
-    R_ocr = (int)(F_CPU / (2 * CLK_DIV * R_pulse_freq));
+    L_rpm = left_speed*60/(PI*WHEEL_WIDTH);
+    R_rpm = right_speed*60/(PI*WHEEL_WIDTH);
 
     if (left_speed > 0)
     {
-        L_dir = false;
+        L_dir = true;
     }
     else
     {
-        L_dir = true;
+        L_dir = false;
     }
 
     if (right_speed > 0)
     {
-        R_dir = false;
+        R_dir = true;
     }
     else
     {
-        R_dir = true;
+        R_dir = false;
     }
 
-    ROS_INFO("L_tcnt: %d, L_dir: %d, R_tcnt: %d, R_dir: %d\n", L_ocr, L_dir, R_ocr, R_dir);
-    configMotor((std::uint8_t)L_ocr, L_dir, (std::uint8_t)R_ocr, R_dir);
-
+    setLeftMotorSpeed((std::uint8_t) L_rpm);
+    setLeftMotorDirection((std::uint8_t) L_dir);
+    setRightMotorSpeed((std::uint8_t) R_rpm);
+    setRightMotorDirection((std::uint8_t) R_dir);
 }
 
 
